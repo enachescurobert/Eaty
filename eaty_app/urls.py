845565221494cd890d39aae7036e56f1d6c19b51
Robert <RobertEnachescu@artal.fr>
2018-08-06 17:13:32 +0200

    Commented code that was not needed

--- eaty_app/urls.py
+++ eaty_app/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+"""
 from django.conf import settings
 from django.conf.urls.static import static
-
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Product/', include('eaty_product.urls')),
     path('Purchase/', include('eaty_purchase.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
