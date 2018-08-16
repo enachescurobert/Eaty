f765bb17243da65a7da3a65548c816277b1d315f
Robert <RobertEnachescu@artal.fr>
2018-08-16 17:39:18 +0200

    Complete Rest Api

--- eaty_product/urls.py
+++ eaty_product/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from eaty_product.views import ProductViewSet, ProductTypeViewSet
+from django.urls import path,re_path
+# from rest_framework.urlpatterns import format_suffix_patterns
+
+# from django.conf.urls import url
+
+# from rest_framework.routers import DefaultRouter
+# from eaty_product.views import ProductViewSet, ProductTypeViewSet
 from . import views
+
+urlpatterns = [
+    re_path(r'^producttypes/$', views.ProductTypeList.as_view()),
+    re_path(r'^producttypes/(?P<pk>[0-9]+)/$', views.ProductTypeDetail.as_view()),
+]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
 """
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,8 +23,8 @@ urlpatterns = [
 # ]
 
 
-router = DefaultRouter()
-router.register(prefix='product', viewset=ProductViewSet)
-router.register(prefix='producttype', viewset=ProductTypeViewSet)
+# router = DefaultRouter()
+# router.register(prefix='product', viewset=ProductViewSet)
+# router.register(prefix='producttype', viewset=ProductTypeViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
--- eaty_product/views.py
+++ eaty_product/views.py
@@ -1,43 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.generic.base import TemplateView
-from rest_framework import viewsets
 from eaty_product.models import ProductType, Product
-from eaty_product.serializers import ProductTypeSerializer, ProductSerializer
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from eaty_product.serializers import ProductSerializer,ProductTypeSerializer
+from rest_framework import generics
 
-class ProductTypeViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing ProductType objects """
+class ProductTypeList(generics.ListCreateAPIView):
     queryset = ProductType.objects.all()
     serializer_class = ProductTypeSerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Product objects """
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
-# def index(request):
-#     #return HttpResponse("You are at /Product/ index")
-#     return render(request, 'index.html')
-#     #template = loader.get_template('eaty_product/index.html')
-
-
-# @csrf_exempt
-# def products_list(request):
-#     """
-#     List all code products, or create a new product.
-#     """
-#     if request.method == 'GET':
-#         products = ProductType.objects.all()
-#         serializer = ProductTypeSerializer(products, many=True)
-#         return JsonResponse(serializer.data, safe=False)
-
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = ProductTypeSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
+class ProductTypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
