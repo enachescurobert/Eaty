5688b68287377b0d9087b5066b401d95a0d81f40
Robert <RobertEnachescu@artal.fr>
2018-08-22 14:10:01 +0200

    Working Wishlist

--- statics/app/pages/wishlist/wishlist.component.js
+++ statics/app/pages/wishlist/wishlist.component.js
@@ -5,30 +5,15 @@ angular
 .module('WishList')
 
 .component('produsList', {
-    controller: ProdusListController,  
-    templateUrl: 'static/app/pages/wishlist/templates/produse.html'
-    })
- .component('produsView',{
-    controller: ProdusViewController,
-    templateUrl: 'static/app/pages/wishlist/templates/produs-view.html'
-    })
-  .component('produsCreate',{
-    controller: ProdusCreateController,
-    templateUrl: 'static/app/pages/wishlist/templates/produs-add.html',
-    })
-  .component('produsEdit',{
-    controller: ProdusEditController,
-    templateUrl: 'static/app/pages/wishlist/templates/produs-edit.html',
-    })
-
-function ProdusListController(popupService, $window, Produs) 
+    templateUrl: 'static/app/pages/wishlist/templates/produse.html',
+    controller: function($scope,popupService, $window, Produs) 
     {
     // GET : Take everything
-    var $ctrl = this;
-    $ctrl.produse = Produs.query();
+
+    $scope.produse = Produs.query();
   
   
-    $ctrl.deleteProdus = function(produs) { // Delete a produs. Issues a DELETE to /api/produse/:id
+    $scope.deleteProdus = function(produs) { // Delete a produs. Issues a DELETE to /api/produse/:id
       if (popupService.showPopup('Really delete this?')) {
         produs.$delete(function() {
           $window.location.href = ''; //redirect to home
@@ -37,51 +22,115 @@ function ProdusListController(popupService, $window, Produs)
       }
     };
   // controllerAs: 'ProdusListController'
-}
+}  
+})
+
+ .component('produsView',{
+    templateUrl: 'static/app/pages/wishlist/templates/produs-view.html',
+    controller: function($scope,$stateParams, Produs) {
+      $scope.produs = Produs.get({id:$stateParams.id}); //Get a single produs.Issues a GET to /api/produse/:id
+    }
+    // controllerAs: 'ProdusViewController'
+
+  })
+
+
+  .component('produsCreate',{
+    templateUrl: 'static/app/pages/wishlist/templates/produs-add.html',
+    controller: function($scope, $state, $stateParams, Produs) {
+      $scope.produs = new Produs();  //create new produs instance. Properties will be set via ng-model on UI
+      
+      $scope.addProdus = function() { //create a new produs. Issues a POST to /api/produse
+        $scope.produs.$save(function() {
+          $state.go('wishlist'); // on success go back to home i.e. produse state.
+        });
+      };
+      
+      // controllerAs: 'ProdusCreateController'
+      },
+  })
+
+  
+  .component('produsEdit',{
+    templateUrl: 'static/app/pages/wishlist/templates/produs-edit.html',  
+    controller: function($scope, $state, $stateParams, Produs) {
+   
+     $scope.updateProdus = function() { //Update the edited produs. Issues a PUT to /api/produse/:id
+     $scope.produs.$update(function() {
+     $state.go('wishlist'); // on success go back to home i.e. produse state.
+     });
+   };
+   
+   $scope.loadProdus = function() { //Issues a GET request to /api/produse/:id to get a produs to update
+   $scope.produs = Produs.get({id:$stateParams.id});
+   };
+   
+   $scope.loadProdus(); // Load a produs which can be edited on UI
+   }
+   // controllerAs: 'ProdusEditController',
+  })
+
+// function ProdusListController(popupService, $window, Produs) 
+//     {
+//     // GET : Take everything
+//     var $scope = this;
+//     $scope.produse = Produs.query();
+  
+  
+//     $scope.deleteProdus = function(produs) { // Delete a produs. Issues a DELETE to /api/produse/:id
+//       if (popupService.showPopup('Really delete this?')) {
+//         produs.$delete(function() {
+//           $window.location.href = ''; //redirect to home
+//           // $window.location.reload();  //same shit     
+//          });
+//       }
+//     };
+//   // controllerAs: 'ProdusListController'
+// }
 
 
 
 
-function ProdusViewController($stateParams, Produs) {
-  var $ctrl = this;
-  $ctrl.produs = Produs.get({id:$stateParams.id}); //Get a single produs.Issues a GET to /api/produse/:id
-}
-// controllerAs: 'ProdusViewController'
+// function ProdusViewController($stateParams, Produs) {
+//   var $scope = this;
+//   $scope.produs = Produs.get({id:$stateParams.id}); //Get a single produs.Issues a GET to /api/produse/:id
+// }
+// // controllerAs: 'ProdusViewController'
 
 
 
 
-function ProdusCreateController($state, $stateParams, Produs) {
-var $ctrl = this;
-$ctrl.produs = new Produs();  //create new produs instance. Properties will be set via ng-model on UI
+// function ProdusCreateController($state, $stateParams, Produs) {
+// var $scope = this;
+// $scope.produs = new Produs();  //create new produs instance. Properties will be set via ng-model on UI
 
-$ctrl.addProdus = function() { //create a new produs. Issues a POST to /api/produse
-  $ctrl.produs.$save(function() {
-    $state.go('wishlist'); // on success go back to home i.e. produse state.
-  });
-};
+// $scope.addProdus = function() { //create a new produs. Issues a POST to /api/produse
+//   $scope.produs.$save(function() {
+//     $state.go('wishlist'); // on success go back to home i.e. produse state.
+//   });
+// };
 
-// controllerAs: 'ProdusCreateController'
-}
+// // controllerAs: 'ProdusCreateController'
+// }
 
 
 
-function ProdusEditController($state, $stateParams, Produs) {
-   var $ctrl = this;
+// function ProdusEditController($state, $stateParams, Produs) {
+//    var $scope = this;
 
-  $ctrl.updateProdus = function() { //Update the edited produs. Issues a PUT to /api/produse/:id
-  $ctrl.produs.$update(function() {
-  $state.go('wishlist'); // on success go back to home i.e. produse state.
-  });
-};
+//   $scope.updateProdus = function() { //Update the edited produs. Issues a PUT to /api/produse/:id
+//   $scope.produs.$update(function() {
+//   $state.go('wishlist'); // on success go back to home i.e. produse state.
+//   });
+// };
 
-$ctrl.loadProdus = function() { //Issues a GET request to /api/produse/:id to get a produs to update
-$ctrl.produs = Produs.get({id:$stateParams.id});
-};
+// $scope.loadProdus = function() { //Issues a GET request to /api/produse/:id to get a produs to update
+// $scope.produs = Produs.get({id:$stateParams.id});
+// };
 
-$ctrl.loadProdus(); // Load a produs which can be edited on UI
-}
-// controllerAs: 'ProdusEditController'
+// $scope.loadProdus(); // Load a produs which can be edited on UI
+// }
+// // controllerAs: 'ProdusEditController'
 
 
 
