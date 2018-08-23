bf929c901efd42be4c7c7ee508e4b092cef216b8
Robert <RobertEnachescu@artal.fr>
2018-08-23 09:43:36 +0200

    Intendant page complete

--- eaty_purchase/serializers.py
+++ eaty_purchase/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'groups')
+        fields = ('id', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.ModelSerializer):
