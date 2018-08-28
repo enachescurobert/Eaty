9aebd8a63493a80530a12388e4f524aa35f5b337
Robert <RobertEnachescu@artal.fr>
2018-08-28 11:27:04 +0200

    Create Session + User Rest API

--- eaty_purchase/serializers.py
+++ eaty_purchase/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import serializers, status
 from eaty_purchase.models import Session
 
 
@@ -15,7 +15,22 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class SessionSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserSerializer(required=True)
     class Meta:
         model = Session
         fields = ('id','coffe','cake','user')
+
+    def create(self, validated_data):
+        """
+        Overriding the default create method of the Model serializer.
+        :param validated_data: data containing all the details of student
+        :return: returns a successfully created student record
+        """
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        student, created = Session.objects.update_or_create(user=user,
+                            coffe=validated_data.pop('coffe'),
+                            cake=validated_data.pop('cake'))
+        return student
+
+    
