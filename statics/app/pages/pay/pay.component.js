abf6fe09fd88fb4c4a38ec18236bbc25e0f3abab
ciprianaelena <stefancipriana@yahoo.com>
2018-09-24 16:20:16 +0200

    PayPal button on Pay page

--- statics/app/pages/pay/pay.component.js
+++ statics/app/pages/pay/pay.component.js
@@ -4,10 +4,46 @@
 angular.
   module('pay').
   component('pay', {
-    templateUrl: 'static/app/pages/pay/pay.template.html',
-   
+    controller: PayButtonController,
+    templateUrl: 'static/app/pages/pay/pay.template.html',   
   
-  });
+  })
+
+  function PayButtonController(){
+    paypal.Button.render({
+      // Configure environment
+      env: 'sandbox',
+      client: {
+        sandbox: 'demo_sandbox_client_id',
+        production: 'demo_production_client_id'
+      },
+      // Customize button (optional)
+      locale: 'en_US',
+      style: {
+        size: 'small',
+        color: 'gold',
+        shape: 'pill',
+      },
+      // Set up a payment
+      payment: function(data, actions) {
+        return actions.payment.create({
+          transactions: [{
+            amount: {
+              total: '0.01',
+              currency: 'USD'
+            }
+          }]
+        });
+      },
+      // Execute the payment
+      onAuthorize: function(data, actions) {
+        return actions.payment.execute().then(function() {
+          // Show a confirmation message to the buyer
+          window.alert('Thank you for your purchase!');
+        });
+      }
+    }, '#paypal-button');
+  }
 
 
 
