759f2aac754fc6ae7cc01ef97aaf0a113de4af4c
Robert <RobertEnachescu@artal.fr>
2018-09-27 10:31:50 +0200

    Table Export as CSV

--- statics/app/components/session-row/session-row.controllers.js
+++ statics/app/components/session-row/session-row.controllers.js
@@ -95,4 +95,42 @@ angular.module('sessionControllers', [])
       $scope.loadSession(); // Load a session which can be edited on UI     
     }
   )
+  .controller('DownloadTable', function($scope) {
+    $scope.name = 'World';
+    
+          $scope.exportAction = function (option) {
+            switch (option) {
+                case 'pdf': $scope.$broadcast('export-pdf', {}); 
+                    break; 
+                case 'excel': $scope.$broadcast('export-excel', {});
+                    break; 
+                case 'doc': $scope.$broadcast('export-doc', {});
+                    break;
+                case 'csv': $scope.$broadcast('export-csv', {});
+                    break;
+                default: console.log('no event caught'); 
+            }
+        }
+              
+  })
+         .directive('exportTable', function(){
+            var link = function ($scope, elm, attr) {
+              $scope.$on('export-pdf', function (e, d) {
+                  elm.tableExport({ type: 'pdf', escape: false });
+              });
+              $scope.$on('export-excel', function (e, d) {
+                  elm.tableExport({ type: 'excel', escape: false });
+              });
+              $scope.$on('export-doc', function (e, d) {
+                  elm.tableExport({ type: 'doc', escape: false });
+              });
+              $scope.$on('export-csv', function (e, d) {
+                  elm.tableExport({ type: 'csv', escape: false });
+              });
+          }
+          return {
+              restrict: 'C',
+              link: link
+          }
+  });
   
