6ab081efc0d029a6402e7ca52ca87d1496311363
Radu Alexandra <radualexandra268@yahoo.com>
2018-12-30 16:28:30 +0200

    Updated the User Serializer + Authentication

--- eaty_purchase/serializers.py
+++ eaty_purchase/serializers.py
@@ -2,66 +2,44 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from rest_framework import serializers, status
 from eaty_purchase.models import Session
+from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, *args, **kwargs):
+        user = super().create(*args, **kwargs)
+        p = user.password
+        user.set_password(p)
+        user.save()
+        return user
+
+    def update(self, *args, **kwargs):
+        user = super().update(*args, **kwargs)
+        p = user.password
+        user.set_password(p)
+        user.save()
+        return user
+
     class Meta:
-        id = serializers.IntegerField(required=False)
-        model = User
-        fields = ('id','first_name','last_name', 'username', 'email','groups')
-        # read_only_fields=('username',)
-
-        # read_only_fields=('username',)
-        # depth = 1
-        extra_kwargs = {
-            'username': {
-                'validators': [UnicodeUsernameValidator()],
-            }
-}
-
+        model = get_user_model()
+        fields = "__all__" 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = "__all__"
-        # exclude = ('permissions',)
-        # read_only_fields=('permissions',)
-        # depth = 1
+
 
 class SessionSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField(required=True)
-    # user = UserSerializer(required=True)
-    # user = UserSerializer(read_only=True)
-    # user = UserSerializer()
+
     user = UserSerializer()
     class Meta:
         model = Session
         fields = ('id','coffe','cake','user')
         read_only_fields=('first_name','last_name','username')
 
-        # fields = "__all__"
-        # depth = 1   
-   
-   
-    # def create(self, validated_data):
-    #     """
-    #     Overriding the default create method of the Model serializer.
-    #     :param validated_data: data containing all the details of student
-    #     :return: returns a successfully created student record
-    #     """
-    #     user_data = validated_data.pop('user')
-    #     user = UserSerializer.create(UserSerializer(), validated_data=user_data)
-    #     student, created = Session.objects.update_or_create(user=user,
-    #                         coffe=validated_data.pop('coffe'),
-    #                         cake=validated_data.pop('cake'))
-    #     return student
 
     def create(self, validated_data):
-        """
-        Overriding the default create method of the Model serializer.
-        :param validated_data: data containing all the details of student
-        :return: returns a successfully created student record
-        """
         user_data = validated_data.pop('user')
         user = UserSerializer.create(UserSerializer(), validated_data=user_data)
         student, created = Session.objects.update_or_create(user=user,
@@ -77,15 +55,5 @@ class SessionSerializer(serializers.ModelSerializer):
         instance.save()
 
         sessions = validated_data.get('sessions')
-
-        # for session in sessions:
-        #     session_id = session.get('id', None)
-        #     if session_id:
-        #         inv_session = Session.objects.get(id=session_id, invoice=instance)
-        #         inv_session.coffe = session.get('coffe', inv_session.coffe)
-        #         inv_session.cake = session.get('cake', inv_session.cake)
-        #         inv_session.save()
-        #     else:
-        #         Session.objects.create(account=instance, **session)
 
         return instance
